432dca4a12a07ac6c4a016d8fc69a17211df1d2e
Isaque Gabriel <isaquebbg@gmail.com>
2025-02-05 19:46:59 -0300

    excluindo rotas

--- src/app.js
+++ src/app.js
@@ -15,20 +15,4 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index])
-})
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-})
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro cadastrado com sucesso');
-})
-
 export default app;
